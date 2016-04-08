3fba803470cdfe6495753139990fe6d9f4adaf4e
Scisco <scisco7@gmail.com>
2016-04-08 13:57:36 -0400

    tests for landsat and sentinel

--- tests/base.py
+++ tests/base.py
@@ -1,3 +1,4 @@
+import re
 import os
 import glob
 import unittest
@@ -5,27 +6,35 @@ import unittest
 
 class BaseTest(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self, path='samples/landsat8', extension='tif', scenes=None):
 
-        self.test_dir = os.path.join(os.path.dirname(__file__), 'samples/landsat8')
-        self.files = glob.glob(os.path.join(self.test_dir, '*.tif'))
+        self.test_dir = os.path.join(os.path.dirname(__file__), path)
+        self.files = glob.glob(os.path.join(self.test_dir, '*.%s' % extension))
 
-        self.landsat8 = {
-            'B1': 'coastal',
-            'B2': 'blue',
-            'B3': 'green',
-            'B4': 'red',
-            'B5': 'nir',
-            'B6': 'swir1',
-            'B7': 'swir2',
-            'B8': 'pan',
-            'B9': 'cirrus',
-            'BQA': 'quality'
-        }
+        if not scenes:
+            self.scenes = {
+                'B1': 'coastal',
+                'B2': 'blue',
+                'B3': 'green',
+                'B4': 'red',
+                'B5': 'nir',
+                'B6': 'swir1',
+                'B7': 'swir2',
+                'B8': 'pan',
+                'B9': 'cirrus',
+                'BQA': 'quality'
+            }
+        else:
+            self.scenes = scenes
+
+        print(self.scenes)
+        print(self.files)
 
         self.file_dict = {}
 
         for f in self.files:
-            band = f.split('_')[-1].split('.')[0]
-            if band in self.landsat8:
-                self.file_dict[f] = [self.landsat8[band]]
+            search = re.search('(B.{1,3})\.', f)
+            if search:
+                band = search.group(0).replace('.', '')
+                if band in self.scenes:
+                    self.file_dict[f] = [self.scenes[band]]
--- tests/test_landsat8.py
+++ tests/test_landsat8.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-from test_scene import _BaseTestScene
-from sprocess.landsat8 import Landsat8Scene
+# from test_scene import _BaseTestScene
+# from sprocess.landsat8 import Landsat8Scene
 
 
-class _TestLandsat8(_BaseTestScene):
+# class _TestLandsat8(_BaseTestScene):
 
-    scene_class = Landsat8Scene
-    sceneid = 'LC80090612015251LGN00'
+#     scene_class = Landsat8Scene
+#     sceneid = 'LC80090612015251LGN00'
 
-    @classmethod
-    def setUpClass(cls):
-        """ Get test image if not present """
-        cls.input_dir = '/home/mhanson/landsat/downloads/%s' % cls.sceneid
+#     @classmethod
+#     def setUpClass(cls):
+#         """ Get test image if not present """
+#         cls.input_dir = '/home/mhanson/landsat/downloads/%s' % cls.sceneid
