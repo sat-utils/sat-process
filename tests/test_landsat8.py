fa7fff76f5130744ad9576b4e0f2bb892f481dc0
Scisco <scisco7@gmail.com>
2016-04-25 13:10:55 -0400

    more tests for landsat

--- tests/test_landsat8.py
+++ tests/test_landsat8.py
@@ -1,6 +1,7 @@
 import unittest
 from stestdata import TestData
 from sprocess.landsat8 import Landsat8
+from sprocess.errors import SatProcessError
 
 
 class TestProduct(unittest.TestCase):
@@ -14,3 +15,29 @@ class TestProduct(unittest.TestCase):
         scene = Landsat8(self.filenames)
         self.assertEqual(len(scene.bandnames()), len(scene.filenames()))
         self.assertEqual(scene.bandnames()[0], 'coastal')
+
+    def test_ndvi(self):
+        scene = Landsat8(self.filenames)
+        self.assertEquals(scene.band_numbers, 10)
+
+        scene.ndvi()
+        self.assertEquals(scene.band_numbers, 11)
+        self.assertTrue('ndvi' in scene.bands)
+
+    def test_ndvi_incorrect_bands(self):
+        scene = Landsat8(self.filenames)
+        self.assertEquals(scene.band_numbers, 10)
+
+        scene2 = scene.select(['red', 'blue', 'green'])
+
+        try:
+            scene2.ndvi()
+        except SatProcessError as e:
+            self.assertEquals(e.message, 'nir band is not provided')
+
+        scene2 = scene.select(['nir', 'blue', 'green'])
+
+        try:
+            scene2.ndvi()
+        except SatProcessError as e:
+            self.assertEquals(e.message, 'red band is not provided')
