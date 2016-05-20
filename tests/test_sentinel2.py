6f7dffa25422676dcaf49018f9e17715126ef4d8
Scisco <scisco7@gmail.com>
2016-05-20 15:09:01 -0400

    more tests for sentinel

--- tests/test_sentinel2.py
+++ tests/test_sentinel2.py
@@ -1,10 +1,23 @@
+import os
+import shutil
 import unittest
+import rasterio
+import numpy as np
+from tempfile import mkdtemp
 from stestdata import TestData
 from sprocess.sentinel2 import Sentinel2
 from sprocess.errors import SatProcessError
 
 
 class TestProduct(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.tmp = mkdtemp()
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.tmp)
 
     def setUp(self):
         self.t = TestData('sentinel2')
@@ -15,6 +28,33 @@ class TestProduct(unittest.TestCase):
         scene = Sentinel2(self.filenames)
         self.assertEqual(len(scene.bandnames()), len(scene.filenames()))
         self.assertEqual(scene.bandnames()[0], 'coastal')
+
+    def test_color_correction(self):
+        scene = Sentinel2(self.filenames)
+        scene.set_bandnames(self.bandnames)
+
+        bands = ['red', 'green', 'blue']
+        rgb = scene.select(bands)
+
+        scene = scene.color_correction(bands=bands)
+        for key in bands:
+            self.assertFalse(np.array_equal(rgb[key], scene[key]))
+
+    def test_true_color(self):
+        scene = Sentinel2(self.filenames)
+        scene.set_bandnames(self.bandnames)
+        self.assertEquals(scene.nbands(), 8)
+
+        f = os.path.join(self.tmp, 'tcolor.tif')
+
+        scene.color_correction(2).true_color(f)
+
+        # open file and make sure it has three bands
+        with rasterio.drivers():
+            with rasterio.open(f, 'r') as src:
+                self.assertEquals(len(src.indexes), 3)
+                shutil.copyfile(f, 'set.tif')
+                # self.assertTrue(np.array_equal(src.read(1), scene['red'].read()))
 
     def test_ndvi(self):
         scene = Sentinel2(self.filenames)
