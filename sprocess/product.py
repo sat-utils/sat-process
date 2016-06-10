79994baf6072a53d3f51913a6d117a4fb0a2c386
Matthew Hanson <matt.a.hanson@gmail.com>
2016-06-10 16:23:14 -0400

    refactor products and scenes

--- sprocess/product.py
+++ sprocess/product.py
@@ -2,81 +2,118 @@
     Product classes which represent files on disk and processing required
 """
 
+import os
+import re
+from gippy import GeoImage
 from gippy.algorithms import indices
 from errors import SatProcessError
 
 
 class Product(object):
     """ A Product is some input (either files, or another series of Products)
-        and some processing performed on that input. """
+        and some processing performed on that input. Products are used as mixins
+        along with a gippy.GeoImage """
 
-    description = 'Product Base Class'
     name = 'product'
+    description = 'Product Base Class'
 
     # dependencies in the form {product: [bands]}
-    dependencies = {}
-
-    # map of original band names to common bandnames
-    _bandmap = {}
-
-    def product_name(self, method):
-        return method + '_' + self.basename()
-
-
-class BaseIndices(Product):
-    description = 'Base class for Indices'
     dependencies = []
 
-    def process(self, method, path=None):
-        args = [self, [method]]
+    _bandmap = {}
 
-        if path:
-            args.append(path)
+    """ Regular expression for matching product files with 2 groups: basename and band """
+    _pattern = r'(.*)_(.*)\..*'
 
-        new_image = indices(*args)
-        return self.__class__(new_image)
+    def __init__(self, scene):
+        """ Initialize a product with a Scene, which contains other products """
+        # products don't exist on their own, they are owned by a parent Scene
+        self.scene = scene
+        # start with no GeoImage
+        self.geoimg = None
+
+    def open(self, filenames, **kwargs):
+        """ Open series of files as a product """
+        if len(filenames) > 0 and filenames is not None:
+            self.geoimg = GeoImage.open(filenames, **kwargs)
+        return self.geoimg
+
+    def process(self, filename=None, **kwargs):
+        """ Check if already exists or process and return GeoImage """
+        self.filename = self.get_filename() if filename is None else filename
+        # geoimg will be None if not already processed - child should do processing
+        return self.geoimg
+
+    def get_filename(self):
+        """ Return an appropriate output filename for this product """
+        return os.path.join(self.scene.outpath, self.scene.basename + '_%s' % self.name)
+
+    def check_bands(self, geoimg):
+        """ Check if all bands required for product are in this geoimg or throw error """
+        if not geoimg.bands_exist(self.dependencies):
+            raise SatProcessError('%s requires bands: %s' % (self.name, ' '.join(self.dependencies)))
+
+    @classmethod
+    def parse_filename(cls, filename):
+        """ Split out basename and bandname (remapped if _bandmap) """
+        m = re.match(cls._pattern, os.path.basename(filename))
+        basename = m.group(1)
+        bandname = cls._bandmap.get(m.group(2), m.group(2))
+        return basename, bandname
 
 
-class TrueColor(BaseIndices):
-
-    def true_color(self, path=None, dtype='byte'):
-        required_bands = ['red', 'green', 'blue']
-        args = [path]
-        kwargs = {}
-
-        # make sure red, green, blue is present
-        self.has_bands(required_bands)
-        rgb = self.select(required_bands)
-
-        if dtype:
-            kwargs['dtype'] = dtype
-
-        if dtype in ['uint8', 'byte']:
-            rgb = rgb.autoscale(1, 255)
-
-        if path:
-            rgb.save(*args, **kwargs)
-        return rgb
+class DigitalCounts(Product):
+    name = 'dc'
+    description = 'Digital counts'
 
 
-class NDVI(BaseIndices):
+class TOA(Product):
+    """ Top of the atmosphere reflectance example """
+    name = 'toa'
+    description = 'Top of the Atmosphere Reflectance'
+
+    def process(self, **kwargs):
+        """ Get TOA reflectance """
+        dc = self.scene.dc()
+        # convert from digital counts to reflectance
+        return dc
+
+
+class NDVI(Product):
+    name = 'ndvi'
     description = 'Normalized Difference Vegetation Index (NDVI) from TOA reflectance'
-    ndvi_enabled = False
 
-    def ndvi(self, path=None):
-        # Make sure band red and nir are present
-        if 'nir' not in self.bands:
-            raise SatProcessError('nir band is not provided')
+    dependencies = ['nir', 'red']
 
-        if 'red' not in self.bands:
-            raise SatProcessError('red band is not provided')
-
-        return self.process('ndvi', path)
+    def process(self, **kwargs):
+        geoimg = self.scene.toa()
+        self.check_bands(geoimg)
+        return indices(geoimg, ['ndvi'])
 
 
-class EVI(BaseIndices):
-    description = 'EVI'
-    evi_enabled = False
+class EVI(Product):
+    name = 'evi'
+    description = 'Enhanced Vegetation Index'
 
-    def evi(self, path=None):
-        return self.process('evi', path)
+    dependencies = ['nir', 'red', 'blue']
+
+    def process(self, **kwargs):
+        geoimg = self.scene.toa()
+        self.check_bands(geoimg)
+        return indices(geoimg, ['evi'])
+
+
+class Color(Product):
+    name = 'color'
+    description = 'Byte-scaled color image with 3 chosen bands'
+
+    def process(self, bands=['red', 'green', 'blue'], **kwargs):
+        geoimg = super(Color, self).process(**kwargs)
+        geoimg = geoimg if geoimg is not None else self.scene.toa()
+        # TODO - need different filenames based on colors used!!!
+        self.filename = self.filename + ''.join([c[0] for c in bands])
+        self.dependencies = bands
+        self.check_bands(geoimg)
+        # should nodata be explicitly set to 0 here?
+        geoimg = geoimg.select(bands).autoscale(1, 255).save(self.filename, dtype='byte')
+        return geoimg
--- sprocess/scene.py
+++ sprocess/scene.py
@@ -1,39 +1,53 @@
-import re
-import gippy
+import product
 from errors import SatProcessError
 
 
-class Scene(gippy.GeoImage):
-    """ Collection of bands for the same scene """
+class Scene(object):
+    """ A scene is a collection of products (which could be multiple bands) yet
+        covers the same (approximate) spatial region and timestamp """
 
-    @property
-    def bands(self):
-        return self.bandnames()
+    # available products - override in children
+    _available_products = [
+        product.DigitalCounts,
+        product.TOA,
+        product.NDVI,
+        product.EVI,
+        product.Color,
+    ]
 
-    @property
-    def band_numbers(self):
-        return self.nbands()
+    default_product = 'dc'
 
-    def get_bandname_from_file(self, value):
+    @classmethod
+    def classname(cls):
+        """ Lowercase string of class name """
+        return cls.__name__.lower()
 
-        # print(value)
-        search = re.search('(B.{1,3})\.', value)
-        if search:
-            return search.group(0).replace('.', '')
+    def __init__(self, filenames, basename=None, outpath='./', **kwargs):
+        """ Create a scene instance with list of products and filenames """
+        # identifier for this scene
+        self.basename = self.classname() if basename is None else basename
+        # directory to store output products
+        self.outpath = outpath
+        # a collection of product class instances
+        # TODO - look at turning into frozen dictionary after __init__
+        self.__products = {p.name: p(self) for p in self._available_products}
+        self.__products[self.default_product].open(filenames, **kwargs)
+
+    def __getattr__(self, attr):
+        """ Get processed products as attributes (e.g., scene.ndvi()) """
+        if attr not in self.__products:
+            raise SatProcessError("%s product not available in %s" % (attr, self.classname()))
         else:
-            return None
+            return self.__products[attr].process
 
-    def select(self, *args, **kwargs):
-        """ Return instance of Scene instead of GeoImage """
-        img = super(Scene, self).select(*args, **kwargs)
-        return self.__class__(img)
+    def __getitem__(self, key):
+        """ Return GeoImage for this product """
+        # TODO - iteratable and other dict-like functions
+        if key not in self.__products:
+            raise SatProcessError("%s product not available in %s" % (key, self.classname()))
+        return self.__products[key]
 
-    def autoscale(self, *args, **kwargs):
-        """ Return instance of Scene instead of GeoImage """
-        img = super(Scene, self).autoscale(*args, **kwargs)
-        return self.__class__(img)
-
-    def has_bands(self, bands):
-        for b in bands:
-            if b not in self.bands:
-                raise SatProcessError('Band %s is required' % b)
+    def available_products(self):
+        """ Get list of available products and descriptions """
+        # TODO - take into account bands, and what bands available in input products, etc
+        return {k: self.__products[k].description for k in self.__products.keys()}
