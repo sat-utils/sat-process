2c4a64ef0a69906d812ec66c010c9f52d22a31da
Matthew Hanson <matt.a.hanson@gmail.com>
2016-06-11 23:13:26 -0400

    rework products and scenes to allow for multiple products of same underlying product type

--- satprocess/product.py
+++ satprocess/product.py
@@ -18,12 +18,7 @@ class Product(object):
     description = 'Product Base Class'
 
     # dependencies in the form {product: [bands]}
-    dependencies = []
-
-    _bandmap = {}
-
-    """ Regular expression for matching product files with 2 groups: basename and band """
-    _pattern = r'(.*)_(.*)\..*'
+    dependencies = {}
 
     def __init__(self, scene):
         """ Initialize a product with a Scene, which contains other products """
@@ -32,34 +27,35 @@ class Product(object):
         # start with no GeoImage
         self.geoimg = None
 
-    def open(self, filenames, **kwargs):
-        """ Open series of files as a product """
-        if len(filenames) > 0 and filenames is not None:
-            self.geoimg = GeoImage.open(filenames, **kwargs)
+    def open(self, filenames=None, bandnames=None, **kwargs):
+        """ Open series of files {bandname: filename} as a product """
+        if filenames is None:
+            # get the filenames from the scene
+            filenames = self.scene.filenames.values()
+            bandnames = self.scene.filenames.keys()
+        if len(filenames) > 0:
+            self.geoimg = GeoImage.open(filenames, bandnames=bandnames, **kwargs)
         return self.geoimg
+
+    def get_dependencies(self):
+        geoimgs = []
+        for d in self.dependencies:
+            geoimg = self.scene[d].process()
+            # check required bands present
+            if not geoimg.bands_exist(self.dependencies[d]):
+                raise SatProcessError('%s requires bands: %s' % (self.name, ' '.join(self.dependencies)))
+            geoimgs.append(geoimg)
+        return geoimgs
 
     def process(self, filename=None, **kwargs):
         """ Check if already exists or process and return GeoImage """
         self.filename = self.get_filename() if filename is None else filename
-        # geoimg will be None if not already processed - child should do processing
+        # get and return dependencies
         return self.geoimg
 
     def get_filename(self):
         """ Return an appropriate output filename for this product """
         return os.path.join(self.scene.outpath, self.scene.basename + '_%s' % self.name)
-
-    def check_bands(self, geoimg):
-        """ Check if all bands required for product are in this geoimg or throw error """
-        if not geoimg.bands_exist(self.dependencies):
-            raise SatProcessError('%s requires bands: %s' % (self.name, ' '.join(self.dependencies)))
-
-    @classmethod
-    def parse_filename(cls, filename):
-        """ Split out basename and bandname (remapped if _bandmap) """
-        m = re.match(cls._pattern, os.path.basename(filename))
-        basename = m.group(1)
-        bandname = cls._bandmap.get(m.group(2), m.group(2))
-        return basename, bandname
 
 
 class DigitalCounts(Product):
@@ -72,48 +68,58 @@ class TOA(Product):
     name = 'toa'
     description = 'Top of the Atmosphere Reflectance'
 
+    dependencies = {'dc': []}
+
     def process(self, **kwargs):
         """ Get TOA reflectance """
-        dc = self.scene.dc()
-        # convert from digital counts to reflectance
-        return dc
+        super(TOA, self).process(**kwargs)
+        if self.geoimg is None:
+            geoimgs = self.get_dependencies()
+            # convert from digital counts to reflectance
+            self.geoimg = geoimgs[0]
+        return self.geoimg
 
 
 class NDVI(Product):
     name = 'ndvi'
     description = 'Normalized Difference Vegetation Index (NDVI) from TOA reflectance'
 
-    dependencies = ['nir', 'red']
+    dependencies = {'toa': ['nir', 'red']}
 
     def process(self, **kwargs):
-        geoimg = self.scene.toa()
-        self.check_bands(geoimg)
-        return indices(geoimg, ['ndvi'])
+        super(NDVI, self).process(**kwargs)
+        if self.geoimg is None:
+            geoimgs = self.get_dependencies()
+            self.geoimg = indices(geoimgs[0], ['ndvi'], filename=self.filename)
+        return self.geoimg
 
 
 class EVI(Product):
     name = 'evi'
     description = 'Enhanced Vegetation Index'
 
-    dependencies = ['nir', 'red', 'blue']
+    dependencies = {'toa': ['nir', 'red', 'blue']}
 
     def process(self, **kwargs):
-        geoimg = self.scene.toa()
-        self.check_bands(geoimg)
-        return indices(geoimg, ['evi'])
+        super(EVI, self).process(**kwargs)
+        if self.geoimg is None:
+            geoimgs = self.get_dependencies()
+            self.geoimg = indices(geoimgs[0], ['evi'], filename=self.filename)
+        return self.geoimg
 
 
 class Color(Product):
     name = 'color'
     description = 'Byte-scaled color image with 3 chosen bands'
 
+    dependencies = {'toa': []}
+
     def process(self, bands=['red', 'green', 'blue'], **kwargs):
-        geoimg = super(Color, self).process(**kwargs)
-        geoimg = geoimg if geoimg is not None else self.scene.toa()
-        # TODO - need different filenames based on colors used!!!
+        """ Color does not retain the geoimg, since it could be different """
+        super(Color, self).process(**kwargs)
+        # dependent on these bands
+        self.dependencies[self.dependencies.keys()[0]] = bands
+        geoimgs = self.get_dependencies()
         self.filename = self.filename + ''.join([c[0] for c in bands])
-        self.dependencies = bands
-        self.check_bands(geoimg)
         # should nodata be explicitly set to 0 here?
-        geoimg = geoimg.select(bands).autoscale(1, 255).save(self.filename, dtype='byte')
-        return geoimg
+        return geoimgs[0].select(bands).autoscale(1, 255).save(self.filename, dtype='byte')
